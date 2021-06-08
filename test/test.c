c40bd5b3ae5db9529476e8c109dcf42a5e3e3dd9
Nightopas <kamenev2002@gmail.com>
2021-06-08 23:36:18 +0700

    Make tests

--- test/test.c
+++ test/test.c
@@ -2,6 +2,20 @@
 
 #include "HangFileRead.h"
 #include <ctest.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+CTEST(filenametest, Emptyfilename)
+{
+    char* filename = "";
+    ASSERT_NULL(read_file(filename, 10));
+}
+
+CTEST(filenametest, Textfilename)
+{
+    char* filename = "src/text/HangmanText";
+    ASSERT_NOT_NULL(read_file(filename, 10));
+}
 
 int main(int argc, const char** argv)
 {
