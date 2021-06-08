a880ba0a4bbf5b0ff0a17606affc82e89c79603a
Nightopas <kamenev2002@gmail.com>
2021-06-08 23:43:43 +0700

    Remove bad test

--- test/test.c
+++ test/test.c
@@ -11,12 +11,6 @@ CTEST(filenametest, Emptyfilename)
     ASSERT_NULL(read_file(filename, 10));
 }
 
-CTEST(filenametest, Textfilename)
-{
-    char* filename = "src/text/HangmanText";
-    ASSERT_NOT_NULL(read_file(filename, 10));
-}
-
 int main(int argc, const char** argv)
 {
     return ctest_main(argc, argv);
