13e04a1c43a4afeebcf79fc32ccb79ac79a2e41e
Nightopas <kamenev2002@gmail.com>
2021-06-09 02:43:37 +0700

    Remove useless check

--- src/HangGrammarCheck.c
+++ src/HangGrammarCheck.c
@@ -4,14 +4,9 @@
 
 int grammar_check(char user_input)
 {
-    if (strlen(user_input) > 1) {
-        printf("ERROR! MORE THAN ONE LETTER! PLEASE ENTER ONLY ONE LETTER AT "
-               "TIME!");
-        return -1;
-    }
     if ((user_input < 'a') || (user_input > 'z')) {
         printf("ERROR! WRONG TYPE OF INPUT! PLEASE ENTER A LETTER IN LOWER "
-               "CASE ENGLISH FORMAT!");
+               "CASE ENGLISH FORMAT!\n");
         return -1;
     } else {
         return 0;
