d86ad90c8c2c070879512ee3348f0cf581d375eb
Nightopas <kamenev2002@gmail.com>
2021-06-09 04:07:42 +0700

    Add draw_hang_word_status function and a header file

---
+++ src/HangWordStatus.c
@@ -0,0 +1,24 @@
+#include "HangWordStatus.h"
+#include "HangWordStruct.h"
+#include <stdio.h>
+
+int draw_hang_word_status(HangLetter *game_word)
+{
+    if (game_word == NULL) {
+        printf("ERROR! GAME_WORD IS NULL!\n");
+        return -1;
+    }
+    int i = 0;
+    while (game_word[i].h_letter) {
+        if (game_word[i].h_flag == 0) {
+            printf("_ ");
+        } else if (game_word[i].h_flag == 1) {
+            printf("%c ", game_word[i].h_letter);
+        } else {
+            printf("ERROR! GAME_WORD %d LETTER HAS WRONG FLAG!\n", i);
+            return -1
+        }
+        i++;
+    }
+    printf("\n");
+}
---
+++ src/HangWordStatus.h
@@ -0,0 +1,7 @@
+#ifndef HANGWORDSTATUS_H
+#define HANGWORDSTATUS_H
+#include "HangWordStruct.h"
+
+int draw_hang_word_status(HangLetter *game_word);
+
+#endif
