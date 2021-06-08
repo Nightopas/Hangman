dbe5346a76364b3c5ecb0bd11a119461c650eded
Nightopas <kamenev2002@gmail.com>
2021-06-08 18:03:03 +0700

    Make a test function

---
+++ test/test.c
@@ -0,0 +1,18 @@
+#define CTEST_MAIN
+
+#include "HangConsoleDraw.h"
+#include <ctest.h>
+
+void test_hangman_draw_console()
+{
+    for (int i = 0; i < 8; i++) {
+        system("clear");
+        hangman_draw_console(i);
+    }
+}
+
+int main(int argc, const char** argv)
+{
+    test_hangman_draw_console();
+    return ctest_main(argc, argv);
+}
