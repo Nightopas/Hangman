418c988c7e213e556567aabc70e9bc37c1fc6b76
Nightopas <kamenev2002@gmail.com>
2021-06-09 13:56:56 +0700

    Remove draw test

--- test/test.c
+++ test/test.c
@@ -197,17 +197,7 @@ CTEST(gamecycle, WrongState2)
     ASSERT_EQUAL(-1, game_cycle(test_word, &game_state));
 }
 
-void test_hangman_draw_console()
-{
-    for (int i = 0; i < 8; i++) {
-        system("clear");
-        hangman_draw_console(i);
-    }
-    system("clear");
-}
-
 int main(int argc, const char **argv)
 {
-    test_hangman_draw_console();
     return ctest_main(argc, argv);
 }
